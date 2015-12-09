283ac15018d86a94eee4ef7aaec66371b743ae46
Behrooz Tahanzadeh <behrooz.tahanzadeh@gmail.com>
2015-12-09 16:56:51 +0100

    adding interval functionality

--- src/topic_io.py
+++ src/topic_io.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python
-import rospy
-import rostopic
-import sys
-import socket
+import rospy, rostopic, sys, socket, getopt
+from time import time
 
-
+interval = None
+lastTimestamp = None
 
 
 def callback(data, args):
+    global interval, lastTimestamp
+    
+    
+    if interval != None:
+        ts = time()
+        
+        if lastTimestamp == None:
+            lastTimestamp = ts
+        elif (ts - lastTimestamp) <= interval:
+            rospy.loginfo("skipped...")
+            return
+        else:
+            lastTimestamp = ts
+    #if
+    
     content = str(data)
-    rospy.loginfo(args)
+    rospy.loginfo("sending data to: "+str(args))
     
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.sendto(content, args)
@@ -21,7 +35,7 @@ def callback(data, args):
 def listener(argv=None):
     rospy.init_node('itech_topic_io', anonymous=True)
     
-    rospy.logwarn("\n********* ITECH_ROS_IO **********\n")
+    rospy.logwarn("\n********* ITECH_ROS_IO **********")
     
     try:
         topicName,msgClass,ip,port = getArgs(argv)
@@ -40,21 +54,30 @@ def listener(argv=None):
 
 
 def getArgs(argv=None):
+    
+    global interval
+    
     if argv is None:
         argv=sys.argv
-        
-    argv = rospy.myargv(argv)
     
-    rospy.loginfo(argv)
+    argv = rospy.myargv(argv)
     
     topicName = argv[1]
     ip = argv[2]
     port = argv[3]
     
+    if 4<len(argv):
+        interval = float(argv[4])
+        rospy.loginfo("Minimum interval between each message has been set...")
+    else:
+        interval = None
+    
     msgClass, realTopic, msgEval = rostopic.get_topic_class(topicName)
     
     return topicName,msgClass,ip,port
 #eof
+
+
 
 
 if __name__ == '__main__':
